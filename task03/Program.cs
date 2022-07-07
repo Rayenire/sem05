a9d03e5c82fd2c3c594c1b82eb545117855205f3
Rayenire <aeristarg@gmail.com>
2022-07-07 11:06:50 +0300

    Add task03

---
+++ task03/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] array = new int[5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine("Введи число: ");
+int n;
+bool log = false;
+n = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < array.Length; i++)
+{
+    if (n == array[i])
+    {
+        log = true;
+        break;
+    }
+}
+if (log)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
+
+
