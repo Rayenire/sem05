5b6b5f52f4f51a2427339f47555d29c8327d12fc
Rayenire <aeristarg@gmail.com>
2022-07-07 11:40:50 +0300

    Add task05

---
+++ task05/Program.cs
@@ -0,0 +1,18 @@
+﻿int n;
+Console.WriteLine("Напишите кол-во чисел: ");
+n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+Console.WriteLine("Напишите числа: ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int len;
+if (n % 2 == 0){
+    len = n / 2 - 1;
+}
+else {len = n / 2;}
+for (int i = 0; i <= len; i++)
+{
+    Console.WriteLine(array[i] * array[array.Length - i - 1]);
+}
