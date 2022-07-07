bbda3c2ac969d46b32f7c864ddc09d593d4398df
Rayenire <aeristarg@gmail.com>
2022-07-07 10:55:16 +0300

    Add task02

---
+++ task02/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = new int[4];
+    for (int i = 0; i < 4; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+    Console.WriteLine("[" + string.Join(", ", array) + "]");
+for (int i = 0; i < 4; i++)
+{
+    array[i] = array[i] * (-1);
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
