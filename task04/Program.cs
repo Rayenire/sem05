1d4ac395e68a3f7535e89b77d4709ccfe3423fcd
Rayenire <aeristarg@gmail.com>
2022-07-07 11:21:33 +0300

    Add task04

---
+++ task04/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = new int[123];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 101);
+}
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] >= 10 && array[i] <= 99)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
+
