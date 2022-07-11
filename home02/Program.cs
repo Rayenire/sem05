b9176c1b9cd13d40bfe7d3063f4fd7482be5ac1e
Rayenire <aeristarg@gmail.com>
2022-07-11 13:16:08 +0300

    Add home02

---
+++ home02/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+int[] array = new int[10];
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(-10, 11);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+FillArray(array);
+PrintArray(array);
+
+int sum = 0;
+for (int i = 1; i < array.Length;)
+{
+    sum = sum + array[i];
+    i = i+2;
+} 
+Console.Write("Сумма нечётных элементов =  ");
+Console.WriteLine(sum);
