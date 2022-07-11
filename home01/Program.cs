63b6866b90ef3fdf8288aee01ff1a7ccfdd88093
Rayenire <aeristarg@gmail.com>
2022-07-11 12:44:47 +0300

    Add home01

---
+++ home01/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] array = new int[20];
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(100, 1000);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+FillArray(array);
+PrintArray(array);
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+} 
+Console.Write("Количество чётных чисел -");
+Console.WriteLine(count);
